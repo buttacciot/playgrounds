90f3ab5e064661a9a72bf421bddc9de4cd055015
Travis Buttaccio <travis@swiftkick.it>
2016-08-01 20:39:39 -0500

    Solved 2D Array - DS

--- HackerRank.playground/Contents.swift
+++ HackerRank.playground/Contents.swift
@@ -47,138 +47,34 @@ struct Matrix {
             grid[(row * columns) + column] = newValue
         }
     }
-    
-    func isMagicSquare() -> Bool {
-        return
-            self[0, 0] + self[0, 1] + self[0, 2] == 15 &&
-            self[1, 0] + self[1, 1] + self[1, 2] == 15 &&
-            self[2, 0] + self[2, 1] + self[2, 2] == 15 &&
-            self[0, 0] + self[1, 1] + self[2, 2] == 15 &&
-            self[0, 2] + self[1, 1] + self[2, 0] == 15
-    }
-    
-    func missingNumbers() -> [Int] {
-        var all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        for i in 0...2 {
-            for j in 0...2 {
-                let next = self[i, j]
-                if all.contains(next) {
-                    all = all.filter { $0 != next }
-                }
-            }
-        }
-        return all
-    }
-    
-    static func cost(mat1: Matrix, mat2: Matrix) -> Int {
-        var cost = 0
-        for i in 0...2 {
-            for j in 0...2 {
-                cost += abs(mat1[i, j] - mat2[i, j])
-            }
-        }
-        return cost
-    }
 }
 
-var notPerfect = Matrix(rows: 3, columns: 3)
-//notPerfect[0, 0] = 4
-//notPerfect[0, 1] = 9
-//notPerfect[0, 2] = 2
-//notPerfect[1, 0] = 3
-//notPerfect[1, 1] = 5
-//notPerfect[1, 2] = 7
-//notPerfect[2, 0] = 8
-//notPerfect[2, 1] = 1
-//notPerfect[2, 2] = 5
-notPerfect[0, 0] = 4
-notPerfect[0, 1] = 4
-notPerfect[0, 2] = 4
-notPerfect[1, 0] = 4
-notPerfect[1, 1] = 4
-notPerfect[1, 2] = 4
-notPerfect[2, 0] = 4
-notPerfect[2, 1] = 4
-notPerfect[2, 2] = 4
+var mat = Matrix(rows: 6, columns: 6)
+mat[0, 0] = 0; mat[0, 1] = -4; mat[0, 2] = -6; mat[0, 3] = 0; mat[0, 4] = -7; mat[0, 5] = -6
+mat[1, 0] = 0; mat[1, 1] = 1; mat[1, 2] = 0; mat[1, 3] = 0; mat[1, 4] = 0; mat[1, 5] = 0
+mat[2, 0] = 1; mat[2, 1] = 1; mat[2, 2] = 1; mat[2, 3] = 0; mat[2, 4] = 0; mat[2, 5] = 0
+mat[3, 0] = 0; mat[3, 1] = 0; mat[3, 2] = 2; mat[3, 3] = 4; mat[3, 4] = 4; mat[3, 5] = 0
+mat[4, 0] = 0; mat[4, 1] = 0; mat[4, 2] = 0; mat[4, 3] = 2; mat[4, 4] = 0; mat[4, 5] = 0
+mat[5, 0] = 0; mat[5, 1] = 0; mat[5, 2] = 1; mat[5, 3] = 2; mat[5, 4] = 4; mat[5, 5] = 0
 
-var perfect = notPerfect
-if notPerfect[1, 1] != 5 {
-    perfect[1, 1] = 5
-}
+var max = Int.min
 
-let missingNumbers = notPerfect.missingNumbers()
-
-// Corners are evens
-missingNumbers.forEach { number in
-    if number % 2 == 0 {
-        if perfect[0, 0] % 2 != 0 {
-            perfect[0, 0] = number
-        }
-        if perfect[0, 2] % 2 != 0 {
-            perfect[0, 2] = number
-        }
-        if perfect[2, 0] % 2 != 0 {
-            perfect[2, 0] = number
-        }
-        if perfect[2, 2] % 2 != 0 {
-            perfect[2, 2] = number
-        }
-    } else {
-        if perfect[1, 0] % 2 != 1 {
-            perfect[1, 0] = number
-        }
-        if perfect[0, 1] % 2 != 1 {
-            perfect[0, 1] = number
-        }
-        if perfect[2, 1] % 2 != 1 {
-            perfect[2, 1] = number
-        }
-        if perfect[1, 2] % 2 != 1 {
-            perfect[1, 2] = number
+for i in 0...3 {
+    for j in 0...3 {
+        let next = mat[i, j] + mat[i, j+1] + mat[i, j+2] + mat[i+1, j+1] + mat[i+2, j] + mat[i+2, j+1] + mat[i+2, j+2]
+        if next > max {
+            max = next
         }
     }
 }
 
-//print(perfect)
-//print(Matrix.cost(notPerfect, mat2: perfect))
+print(max)
 
-func power(x: UInt64, y: UInt64) -> UInt64 {
-    var temp: UInt64
-    if y == 0 {
-        return 1
-    }
-    temp = power(x, y: y/2)
-    if y%2 == 0 {
-        return temp*temp
-    } else {
-        return x*temp*temp
-    }
+
+var array: NSArray = readLine()!.characters.split(" ").map{Int(String($0))!}
+for j in 0...array.count {
+    mat[j, j] = array[j].integerValue
 }
-
-let growthA = 2
-let growthB = 4
-let mills = power(10, y: 18)
-let mod = power(10, y: 9)
-
-let thing = UInt64(growthA+growthB)
-
-//print(mills)
-//print(power(10, y: 18))
-//let final = power(2, y: 10000)
-
-print(power(10, y: 18))
-
-//print(final)
-//print(Double(final % (pow(10, 9) + 7)))
-
-
-
-
-
-
-
-
-
 
 
 
